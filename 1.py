122de7cec7cd7232c734e277657ce97a571d8719
EkhsonK <korcaevehson@gmail.com>
2024-03-16 15:17:46 +0300

    v3

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import csv
 
 
 # Перекодируем файл с помощью UTF-8
-with open('space.csv',encoding='utf-8') as f, open('space_new.csv','w',newline='') as nf:
+with open('space.csv',encoding='utf-8') as f, open('space_new1.csv','w',newline='') as nf:
     data = list(csv.reader(f,delimiter=','))
     res = csv.writer(nf,delimiter=';')
     for i in data:
