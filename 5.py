7e7b19af620298d64f963a98d1361cecdeb77c7f
EkhsonK <korcaevehson@gmail.com>
2024-03-16 15:17:57 +0300

    v3

--- 5.py
+++ 5.py
@@ -1,10 +1,12 @@
 import csv
 
-alf = sorted('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMйцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁё')
+# Данные для хеширования по имени корабля
+alf = sorted('1234567890-_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMйцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁё')
 d = {alf[i]:i+1 for i in range(len(alf))}
+# функция хеширования
 def hash(s):
     s = s.replace(' ','')
-    p = 71
+    p = 123
     m = (10**9)+9
     i = 0
     hash_value = 0
@@ -14,12 +16,13 @@ def hash(s):
     return int(hash_value % m)
 
 # Открываем файл и записываем новый
-with open('space.csv',encoding='utf-8') as f, open('space_new.txt','w',newline='') as nf:
+with open('space.csv',encoding='utf-8') as f, open('space_new5.csv','w',newline='') as nf:
     data = list(csv.reader(f,delimiter=','))
     res = csv.writer(nf,delimiter=';')
     res.writerow(data[0])
+    # меняем имя корабля на его хеш
     for stroka in data[1:]:
         name = stroka[0]
-        h = hash(fio)
+        h = hash(name)
         stroka[0] = h
         res.writerow(stroka)
